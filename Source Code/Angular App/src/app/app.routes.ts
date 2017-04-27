fa3f0db302217f8d22df8e371f6b76a3bc8950a2
huynhvanthao <thaohv93@gmail.com>
2017-04-27 10:38:28 +0700

    add upload route

--- Source Code/Angular App/src/app/app.routes.ts
+++ Source Code/Angular App/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { TemplatePageComponent } from './template-page/template-page.component';
+import { UploadComponent } from './upload/upload.component';
 import { LoginGuard } from './guards/login.guard';
 
 const routing: Routes = [
     { path: '', component: LoginComponent },
     { path: 'home', component: TemplatePageComponent, canActivate: [LoginGuard] },
+    { path: 'upload', component: UploadComponent },
     { path: '**', component: NotfoundComponent }
 
 ];
